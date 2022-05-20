57c61f2350705dfc6808b0afc8dbfa938b212d54
BloodRedTape <gefestroyale@gmail.com>
2022-05-20 17:23:05 +0300

    Stuff

--- client/cortex.cpp
+++ client/cortex.cpp
@@ -1,33 +1,14 @@
 #include <iostream>
+#include <sstream>
 #include "fs/dir.hpp"
 #include "net/tcp_socket.hpp"
-#include "commit.hpp"
+#include "protocol.hpp"
 
 struct Host {
 	IpAddress Address;
 };
 
-class Server {
-private:
-	// The Truth is here
-	CommitHistory History;
-public:
-	
-	void OnRequest(const Request& request, Responce& responce) {
-
-	}
-};
-
-class Client {
-	CommitHistory History;
-	std::vector<FileAction> LocalChanges;
-public:
-
-	void PushChanges() { }
-
-	void PullHistory() { }
-};
-
 
 int main() {
+
 }
--- server/cortex.cpp
+++ server/cortex.cpp
@@ -1,7 +1,34 @@
 ﻿#include "repository.hpp"
+#include "protocol.hpp"
+#include "fs/dir.hpp"
+#include "commit.hpp"
 #include "net/tcp_socket.hpp"
 
+class Server {
+private:
+	DirRef RepoDir;
+	CommitHistory History;
+public:
+	Server(std::string path):
+		RepoDir(
+			Dir::Create(std::move(path))
+		),
+		History(
+			RepoDir->ReadEntireFile(".history").second
+		)
+	{}
+
+	~Server() {
+		RepoDir->WriteEntireFile(".history", History.ToBinary());
+	}
+
+	void Run() {
+
+	}
+};
+
+
 int main(){
-	Repository("W:\\Dev\\Cortex\\out\\repo\\").Run();
+	Server("W:\\Dev\\Cortex\\out\\server\\").Run();
 	return 0;
 }
