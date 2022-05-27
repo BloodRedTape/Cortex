e09355d09d430be58ce1ecfe70e015480616f6f1
BloodRedTape <gefestroyale@gmail.com>
2022-05-27 15:56:18 +0300

    UdpSocket assertions

--- core/net/udp_socket.cpp
+++ core/net/udp_socket.cpp
@@ -37,6 +37,7 @@ void UdpSocket::Close(){
 }
 
 bool UdpSocket::Bind(IpAddress address, u16 port_hbo) {
+	assert(IsOpen());
 	assert(!IsBound());
 
 	return (m_IsBound = BindImpl(m_Handle, address, port_hbo));
@@ -51,12 +52,14 @@ bool UdpSocket::IsBound()const {
 }
 
 u32 UdpSocket::Send(const void* data, u32 size, IpAddress dst_ip, u16 dst_port_hbo) {
+	assert(IsOpen());
 	m_IsBound = true;
 
 	return SendImpl(m_Handle, data, size, dst_ip, dst_port_hbo);
 }
 
 u32 UdpSocket::Receive(void* data, u32 size, IpAddress& src_ip, u16& src_port_hbo) {
+	assert(IsOpen());
 	assert(IsBound());
 
 	return ReceiveImpl(m_Handle, data, size, src_ip, src_port_hbo);
